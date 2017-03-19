52b10bdc5cfb9e4d8d31046a54b146ae4b0e70dc
Angelique Clery <angeliqueclery@yahoo.fr>
2017-03-19 18:28:37 +0100

    first commit

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,4 +1,17 @@
 // MAIN CONTROLLER
 function mainController($scope) {
-    
+    $scope.users = [];
+    $scope.addUser = function(){
+    var insert = {};
+    insert.lastname   =  $scope.lastname;
+    insert.firstname =  $scope.firstname;
+    insert.email    =  $scope.email;
+    $scope.users.push(insert);
+    $scope.lastname   = "";
+    $scope.firstname = "";
+    $scope.email    = "";
+    $scope.deleteUser = function($index){
+    $scope.users.splice($index,1);
+   };
+ };
 }
